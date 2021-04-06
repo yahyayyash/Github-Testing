9becdf77f23d54df93b80241bb38b0d899ccc781
yahyayyash <yahyayyashaa@gmail.com>
2021-04-06 20:04:37 +0700

    Initial Commit

---
+++ Github Testing/Github_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TestingApp.swift
+//  Github Testing
+//
+//  Created by Yahya Ayyash on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Github_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
